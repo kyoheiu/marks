f0d90544034e65d86887dec70fa7b42b516f5c11
Kyohei Uto <im@kyoheiu.dev>
2023-08-05 06:33:39 +0900

    Detect change on input, textarea efficiently

--- svelte/src/lib/Edit.svelte
+++ svelte/src/lib/Edit.svelte
@@ -6,11 +6,22 @@
   import { save } from "./Upload";
 
   let content = get(state).content;
+  let edited = false;
+
+  const detectChange = () => {
+    edited = true;
+  }
+
+const keyDown = (e: KeyboardEvent) => {
+  if (e.key === "Enter" && e.ctrlKey) {
+   save(content);
+   edited = false;
+  }
+}
 </script>
 
 <Toaster />
-<div class="flex min-h-full flex-col items-center">
-  <div class="flex justify-center">
+<div class="flex min-h-full flex-col justify-center items-center">
     <div
       class="sticky top-12 flex w-full items-center bg-sky-200 p-2"
     >
@@ -18,21 +29,22 @@
         class="w-32 sm:w-80 md:w-96 h-8 border border-zinc-300 px-2 font-mono text-sm"
         bind:value={$state.newName}
         placeholder="file name"
-        on:keydown={(e) => e.key === "Enter" && e.ctrlKey && save(content)}
+    on:input={detectChange}
+    on:keydown={(e) => keyDown(e)}
       />
       <button
-        class="box-border mx-2 w-12 h-6 rounded-full border border-sky-500 bg-white px-2 text-sm text-sky-500"
+        class="box-border mx-2 w-12 h-6 rounded-full border border-sky-500  bg-white hover:bg-sky-500 px-2 text-sm text-sky-500 hover:text-white"
         on:click={reviewItem}
         title="back to view"><i class="ri-arrow-go-back-line" /></button
       >
-      <Upload {content} />
-    </div>
+      <Upload {edited} {content} />
   </div>
   <textarea
     class="h-120 w-64 flex-grow border border-zinc-300 bg-white p-3 font-mono text-sm text-zinc-900 outline-none sm:h-144 sm:w-120 md:w-144"
     contenteditable="true"
     bind:value={content}
     placeholder="Write here."
-    on:keydown={(e) => e.key === "Enter" && e.ctrlKey && save(content)}
+    on:input={detectChange}
+    on:keydown={(e) => keyDown(e)}
   />
 </div>
