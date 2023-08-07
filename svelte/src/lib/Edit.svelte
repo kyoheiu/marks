f41527183b158970a83caee9aafb8dd39c620e98
Kyohei Uto <im@kyoheiu.dev>
2023-08-08 05:20:48 +0900

    Add breakpoint to hover

--- svelte/src/lib/Edit.svelte
+++ svelte/src/lib/Edit.svelte
@@ -78,7 +78,7 @@
     >
     <div class="relative flex items-start">
       <button
-        class="box-border h-6 w-12 rounded-full bg-sky-500 px-2 text-sm text-white hover:bg-sky-600"
+        class="box-border h-6 w-12 rounded-full bg-sky-500 px-2 text-sm text-white md:hover:bg-sky-600"
         on:click={() => save(content)}
         title="save"><i class="ri-file-upload-line" title="save" /></button
       >
